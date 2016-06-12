b5d87e0e2af4c92d145f8b840bcd06c5717886f0
DramonKiller <hecsanbar@gmail.com>
2016-06-12 21:54:27 +0200

    Utilities project added. EnumerableExtensions added.

---
+++ Utilities/EnumerableExtensions/EnumerableExtensionsImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dramonkiller.Utilities.EnumerableExtensions
+{
+    /// <summary>
+    /// Class that contains the enumerable extensions.
+    /// </summary>
+    public static class EnumerableExtensionsImpl
+    {
+        /// <summary>
+        /// Perform an action on each item in the enumerable
+        /// </summary>
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
--- WindowsFormsLibrary/ControlExtensions.cs
+++ WindowsFormsLibrary/ControlExtensions.cs
@@ -7,8 +7,17 @@ using System.Windows.Forms;
 
 namespace Dramonkiller.WindowsFormsLibrary
 {
+    /// <summary>
+    /// Class that contains the control extensions.
+    /// </summary>
     public static class ControlExtensions
     {
+        /// <summary>
+        /// Executes the action checking if the current thread is the UI thread or not. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"> The control. </param>
+        /// <param name="action"> The action method. </param>
         public static void ExecuteThreadSafe<T>(this T control, Action<T> action) where T : Control
         {
             if (control == null)
