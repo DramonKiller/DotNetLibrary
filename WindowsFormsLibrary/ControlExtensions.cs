9d85d8d67bbaaa5787d88dd004df72aabe2cbce1
DramonKiller <hecsanbar@gmail.com>
2016-06-12 21:41:16 +0200

    WindowsFormsLibrary project created. ControlExtensions class created. ExecuteThreadSafe extension method added.

---
+++ WindowsFormsLibrary/ControlExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dramonkiller.WindowsFormsLibrary
+{
+    public static class ControlExtensions
+    {
+        public static void ExecuteThreadSafe<T>(this T control, Action<T> action) where T : Control
+        {
+            if (control == null)
+            {
+                throw new ArgumentNullException("control");
+            }
+
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action); 
+            }
+            else
+            {
+                action.Invoke(control); 
+            }
+        }
+    }
+}
